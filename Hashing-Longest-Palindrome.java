e8419cb572dacfb3ae781aba2e0cb015dd661ea0
Rohanp1196 <54926705+Rohanp1196@users.noreply.github.com>
2019-10-16 11:11:08 -0700

    Problem-3-updated

--- Hashing-Longest-Palindrome.java
+++ Hashing-Longest-Palindrome.java
@@ -10,22 +10,22 @@ class Solution {
         
         int count = 0;
         
-        HashSet<Character> hs = new HashSet<>(); 
+        HashSet<Character> hs = new HashSet<>(); //New Hashset
         
         for (int i=0;i<s.length();i++){
             
-            char ch = s.charAt(i);
+            char ch = s.charAt(i); //Traversing each character of string
             
-            if(hs.contains(ch)){
+            if(hs.contains(ch)){ // If already present in hashset, then remove from hashset and increment count
                 hs.remove(ch);
                 count += 2;
             }
             else{
-                hs.add(ch);
+                hs.add(ch);  //Else add for the first time
             }
         }
-        if(!hs.isEmpty()) return count + 1;
+        if(!hs.isEmpty()) return count + 1; //For 1 occurencec characters, adding one to the count
         else
-            return count;
+            return count;  //If no 1 occurence, then return count;
     }
 }
