d8082d1843d57c34c787bd94b2c816ca2b002f75
priyanshi-1234 <agrawalpriyanshi1@gmail.com>
2019-10-19 16:22:05 +0530

    Update Subarray Sum Equals K.cpp

--- Subarray Sum Equals K.cpp
+++ Subarray Sum Equals K.cpp
@@ -1,5 +1,9 @@
 
 // this is subarray sum equals problem solved in c++
+// here i am initially taking an edge case where 0->1 anf then traversing through the vector calculating the running sum , at the same time 
+// taking the compliment i.e runningsum - reqsum  and then checking whether that running sum is present in the map or not if present then add the required value at corresponding
+// compliment key to the count variable. then we check  whether runningsum is present in map or not if it is than we add 1 to the value of that key.
+// if runningsum not present we make a new pair corrseponding to running sum and assign it a value of  and insert it in the map. then finally return count.
 
 class Solution {
 public:
