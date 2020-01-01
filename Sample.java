bf9e443adf79bc632393882fa56f8f17f964a4fb
poojakittur <gitpoojakittur@gmail.com>
2020-01-01 22:20:12 +0530

    Adding solutions to Hashing-2

--- Sample.java
+++ Sample.java
@@ -1,7 +1,16 @@
 // Time Complexity :
+//Problem_1 : O(n)
+//Problem_2 : O(n)
+//Problem_3 : O(n)
+
 // Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//Problem_1 : O(n)
+//Problem_2 : O(n)
+//Problem_3 : O(1)
+
+
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
