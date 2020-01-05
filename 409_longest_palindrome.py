838b7bab9eddb465c67f35e1e56f778cedf8cbbc
Nikhil Agrawal <nikrasiya@gmail.com>
2020-01-05 08:20:08 -0500

    all problems complete updated as per class

--- 409_longest_palindrome.py
+++ 409_longest_palindrome.py
@@ -5,7 +5,7 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         """
             // Time Complexity: O(n)
-            // Space Complexity: O(n)
+            // Space Complexity: O(1)
             // Did this code successfully run on Leetcode :
                 Yes
             // Any problem you faced while coding this :
