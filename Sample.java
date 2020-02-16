42fceea49196ee63934b1152500e8b1a2d05c2cf
Chaitrali Deshmukh <42701774+ChaiDeshmukh@users.noreply.github.com>
2020-02-16 14:44:52 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,26 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+
+
+
+Problem 3:  Longest Palindrome
+
+Bruteforce:
+Check all possible strings and prepare a power set and then check the largest palindromic string among them.
+
+Time Complexity: 2^n
+Space Complexity: O(n)  --> maybe
+
+
+Using HashSet:
+put each char in hashset, if the char is already present in the string, we found the pair and increament the count by 2
+return count + 1,
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+  
+  
+  
