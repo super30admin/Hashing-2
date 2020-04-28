876ab23e97482760e30945580f43d43a7a8f5d56
Siyami Avci <siyamiavci@gmail.com>
2020-04-27 23:32:05 -0700

    add problems

---
+++ problem1.js
@@ -0,0 +1,20 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// ## Problem1 (https://leetcode.com/problems/subarray-sum-equals-k/)
+// Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+// Example 1:
+
+// Input:nums = [1,1,1], k = 2
+
+// Output: 2
+
+// Note:
+
+// The length of the array is in range [1, 20,000].
+// The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+
---
+++ problem2.js
@@ -0,0 +1,22 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// ## Problem2 (https://leetcode.com/problems/contiguous-array/)
+// Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+// Example 1:
+
+// Input: [0,1]
+// Output: 2
+// Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+// Example 2:
+
+// Input: [0,1,0]
+// Output: 2
+// Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+// Note: The length of the given binary array will not exceed 50,000.
+
+
