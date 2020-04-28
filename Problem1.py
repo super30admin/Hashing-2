d0d6188b7b3f17dd58628bec0e12368bf88cd640
shriyavanvari <vanvari.shriya@gmail.com>
2020-04-28 11:48:34 -0700

    Problem1 and 2 solve

--- Problem1.py
+++ Problem1.py
@@ -2,28 +2,29 @@
 
 # Space Complexity: O(n) for the hash map and the cumulative sum array
 
-# Did it run on Leetcode:
+# Did it run on Leetcode:Yes
 
 # Approach:
 # Calulculate the cumulative sum at each index in the list.
 # If at any point the the dofference between the current cumulative sum and cumulative sum at previous index=target, then we know that all elements
 # after the previous cumulative index including the element at the index of new cumulative sum will give me summation which equals to the target
-# Now we maintain a hashmap wherein if the difference between current sum and target is found,we increment the count by 1
+# Now we maintain a hashmap wherein if the difference between current sum and target is found,we increment the count value in hashmap by 1. the total count of subarray
+# will be current count+ value of the count in hashmap so that we cover all permutattions of the subarrays.
 
 class Solution:
     def subarraySum(self, nums, k):
         count = 0
-        cumsum = [0]
-        sumDict = {}
-        for i in range(0, len(nums)):
-            cumsum.append(cumsum[-1] + nums[i])
-        for i in cumsum:
-            if i - k in sumDict:
-                count += sumDict[i - k]
-            if (i in sumDict):
-                sumDict[i] += 1
+        s = 0
+        sumDict = {0:1}
+
+        for i in nums:
+            s+=i
+            if s - k in sumDict:
+                count += sumDict[s - k]
+            if s in sumDict:
+                sumDict[s] += 1
             else:
-                sumDict[i] = 1
+                sumDict[s] = 1
         return count
 
 
