bc83eb05da0f7163ba3414cffd0ec3f431d8f285
Tejas Jain <tejassjain@gmail.com>
2020-04-28 19:44:51 -0500

    Problem 1 and 2 solved

---
+++ subarray_sum_equals_k.java
@@ -0,0 +1,20 @@
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum =0, count=0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int i=0; i<nums.length; i++)
+        {
+            sum = sum + nums[i];
+            if(map.containsKey(sum - k))
+                count = count + map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) +1);
+        }
+        return count;
+    }
+}
