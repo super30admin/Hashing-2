30a212b3dd8380fdf6948be0347c8d8d103f7181
navnitss <43095390+navnitss@users.noreply.github.com>
2020-05-08 07:36:48 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,22 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity :O(n)
+// Space Complexity :O(26)
+// Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this :
+
 // Your code here along with comments explaining your approach
+//we count the number of even occourences and then multiply by 2;then add one if singular elemnts remain in the Hashset that were not removed before
+public int longestPalindrome(String s) {
+        if(s==null || s.length()==0) return 0;
+        HashSet<Character> hs = new HashSet<Character>();
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(hs.contains(s.charAt(i))){
+                hs.remove(s.charAt(i));
+                count++;
+            }else{
+                hs.add(s.charAt(i));
+            }
+        }
+        if(!hs.isEmpty()) return count*2+1;
+        return count*2;
+}
