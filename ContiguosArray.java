35ca66ad626acb4bb6486fd3183498473b7ba1e9
Prashanth Chintala <pchintala16@gmail.com>
2020-06-01 18:48:00 -0700

    Update ContiguosArray.java

--- ContiguosArray.java
+++ ContiguosArray.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(n)
-// Space Complexity :O(n)
+// Space Complexity :O(n). n is the length of the array
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
