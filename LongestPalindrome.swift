4de850c41c932549072b666baeeb5ba564f57717
karthikprabhu alagu <mail2karthi07@gmail.com>
2020-06-03 06:47:28 -0400

    Hashing-2: Updated class soultion

--- LongestPalindrome.swift
+++ LongestPalindrome.swift
@@ -1,3 +1,59 @@
+//After Class
+
+// Time Complexity : (O)n
+// Space Complexity : (O)1 since character is constant
+// Did this code successfully run on Leetcode : yes
+
+
+//Method 1 - Hashing - use Set. remove the repeated value
+class Solution {
+    func longestPalindrome(_ s: String) -> Int {
+        guard s.length > 0 else {return 0}
+        var set = Set<Character>()
+        var count = 0
+        
+        for c in s {
+            if set.contains(c) {
+              count += 2
+              set.remove(c)  
+            } else {
+                set.insert(c)
+            }
+        }
+        
+        return count + (set.isEmpty == true ? 0 : 1)
+        
+    }
+}
+
+//Method 2 -
+
+class Solution {
+    func longestPalindrome(_ s: String) -> Int {
+        guard s.length > 0 else {return 0}
+        var count = [Character:Int]()
+
+        for val in s {
+            count[val] = (count[val] == nil ? 1 : count[val]!+1)
+        }
+        
+        var result = 0
+        for val in count.values {
+            result += ((val/2) * 2)
+            if result%2 == 0 && val % 2 == 1 {
+                result += 1
+            }
+        }
+        
+        return result
+        
+    }
+}
+
+
+
+
+
 // Time Complexity : (O)n
 // Space Complexity : (O)1
 // Did this code successfully run on Leetcode : No. Wrong answer 
