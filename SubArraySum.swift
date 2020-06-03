990c96fc1f6b066da7b6749fbb49c539a8024319
karthikprabhu alagu <mail2karthi07@gmail.com>
2020-06-02 21:36:10 -0400

    Hashing-2: Updated class aswer

--- SubArraySum.swift
+++ SubArraySum.swift
@@ -1,9 +1,34 @@
 
+//After Class
+
+// Time Complexity : (O)n
+// Space Complexity : (O)n
+// Did this code successfully run on Leetcode : yes
+
+
+class Solution {
+    func subarraySum(_ nums: [Int], _ k: Int) -> Int {
+        var count = 0
+        var sum = 0
+        var map = [Int:Int]()
+        map[0] = 1
+        for num in nums {
+            sum += num
+            if map[sum-k] != nil {
+                count += map[sum-k]!
+            }
+            map[sum] = (map[sum] != nil ? map[sum]! : 0) + 1
+        }
+        return count
+    }
+}
+
 // Time Complexity : (O)n
 // Space Complexity : (O)1
 // Did this code successfully run on Leetcode : No. Wrong answer 
 
 
+/*
 class Solution {
     func subarraySum(_ nums: [Int], _ k: Int) -> Int {
         var numberOfSubArray = 0
@@ -23,3 +48,4 @@ class Solution {
         return numberOfSubArray
     }
 }
+*/
