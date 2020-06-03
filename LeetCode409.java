20378d1432f18de17ad8d4913ec09c044f1ac907
Krutik Tatibandwale <krutik@Krutiks-MacBook-Pro.local>
2020-06-03 13:57:48 -0400

    Completed Sprint Hashing-2

---
+++ LeetCode409.java
@@ -0,0 +1,29 @@
+// Time Complexity : O(n) --> where n is length of string
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return 0;
+        if (s.length() == 1) return 1;
+        
+        HashSet<Character> set = new HashSet<>();
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (set.contains(c)) {
+                set.remove(c);
+                count += 2;
+            }
+            else set.add(c);
+        }
+        
+        if (!set.isEmpty()) count++;    
+        return count;
+    }
+}
