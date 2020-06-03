8601e4ef915770e5ab76117ac0bb9d63800c8eb3
nileshph <nileshpharate96@gmail.com>
2020-06-02 19:18:27 -0700

    Added solution for SubArraySumEqualK problem

--- src/ContiguosArray.java
+++ src/ContiguosArray.java
@@ -15,6 +15,11 @@ Example 2:
 Input: [0,1,0]
 Output: 2
 Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+
+brute force - checking each sub array, do we need to do that? if we maintain running sum and corresponding index then no.
+			  we avoid nested operations using running sum strategy
+Strategy - running sum problem, maintain count, count++ for 1, count-- for 0, maintain running sum at each index, 
+		   if sum is equal at two indices, between those 2 indices we have same number of 1 and 0s
  */
 
 import java.util.HashMap;
@@ -24,9 +29,10 @@ class Solution {
     public int findMaxLength(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
         
-        //map to keep track of count and correspoding index
+        //map to keep track of count and corresponding index
         Map<Integer, Integer> countMap = new HashMap<>();
         
+        //we need this for test edge case e.g {1,0}
         countMap.put(0, -1);
         
         int len = 0, countSoFar = 0;
@@ -39,8 +45,10 @@ class Solution {
                 countSoFar--;
             }
             
-            //if same count is found before, current index and that index will give contigous array
+            //if same count is found before, current index and that index will give contiguous array
             if(countMap.containsKey(countSoFar)) {
+            	// if sum is equal at two indices, between those 2 indices we have same number of 1 and 0s, 
+            	//see if length of that sub array is max
                 len = Math.max(len, i - countMap.get(countSoFar));
             } else
             {
---
+++ src/SubArraySumEqualsK.java
@@ -0,0 +1,69 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+/**
+ * https://leetcode.com/problems/subarray-sum-equals-k/
+ * 
+ * Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+
+Input:nums = [1,1,1], k = 2
+Output: 2
+ 
+
+Constraints:
+
+The length of the array is in range [1, 20,000].
+The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+
+brute force: check for each possible sub array - nested checking, good candidate for running sum
+
+strategy - maintain hashmap of running sum and how many times that running sum occurred, while taking running sum
+take diff of running sum and k and see if that diff is in the map, if yes, sum of sub array starting from the places where
+that difference occurred as running sum and ending at current index will be equal to k
+ *
+ */
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        
+        if(nums == null || nums.length ==0 ) return 0;
+       
+        int count = 0, rSum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        // edge case e.g {3,4} k=7
+        map.put(0, 1);
+        
+        for(int num:nums) {
+        	
+        		//calculate running sum
+            rSum+=num;
+            
+            //take difference
+            int complement = rSum - k;
+            
+            //if difference already occurred, increase count by value corresponding to the
+            //difference from the map -- that value represent how many times running sum seen till now
+            if(map.containsKey(complement)) {
+                count+=map.get(complement);
+            }
+            
+            //if running sum occurred before, increase count, if not put 1 as value as running
+            //sum occurred just now so count 1
+            if(!map.containsKey(rSum)) {
+                map.put(rSum, 0);
+            }
+            map.put(rSum, map.get(rSum) + 1);
+        }
+        
+        return count;
+    }
+}
