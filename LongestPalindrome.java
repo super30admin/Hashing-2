123f4699b10aa1d71964f4790eaefc9c6e7b4592
Arpit Sharma <arpit317@gmail.com>
2020-07-15 15:13:08 -0700

    Update LongestPalindrome.java

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,3 +1,15 @@
+// Time Complexity : O(n)) 
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+// Approach:
+// 1. I used HashSet to store the characters in the given string. 
+// 2. Every time we see a character which is already there in the hashset, we remove it from the hashset, as it forms a pair of characters, which can help make
+// a palindrom and we increase the length of plaindrome with +2.
+// 3. At last, if I see that after all the pairs have been formed, HashSet is not empty, then we add +1 to the length of our palindrome, as we can place any one
+// character in the middle of our palindromic string.
+
 class Solution {
     public int longestPalindrome(String s) {
         HashSet<Character> set = new HashSet<Character>();
