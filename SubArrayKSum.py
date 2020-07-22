509ccf4408a74895d2c52af90b4228131c8ebd65
Balaviknesh Sekar <bala21karthick@gmail.com>
2020-07-22 17:54:40 -0600

    Array 1 - completed

--- SubArrayKSum.py
+++ SubArrayKSum.py
@@ -1,27 +1,27 @@
 # Time Complexity : O(n) where n is the number of integers present in the list
 # Space Complexity : O(n) that takes space to store the keys
-# Did this code successfully run on Leetcode : yes
+# Did this code successfully run on LeetCode : yes
 # Any problem you faced while coding this : no
 
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
-        #we intialize this to start from the zero as the first sum
+        # we initialize this to start from the zero as the first sum
         hashMap = {0: 1}
         cSum = 0
         TotalSubArrays = 0
 
         for num in nums:
 
-            #We will get the cumalative sum
+            # We will get the cumulative sum
             cSum += num
 
-            #if csum - k present in the key means we have a subarray of that value in the hashMap key
+            # if csum - k present in the key means we have a subarray of that value in the hashMap key
             if cSum - k in hashMap.keys():
                 TotalSubArrays += hashMap[cSum - k]
 
-            #finaly we will add that csum to the keys, if not already present else, we will update by adding 1 to it.
+            # finally we will add that csum to the keys, if not already present else, we will update by adding 1 to it.
             if cSum in hashMap.keys():
 
                 hashMap[cSum] += 1
@@ -31,4 +31,3 @@ class Solution:
                 hashMap[cSum] = 1
 
         return TotalSubArrays
-
--- contiguosArray.py
+++ contiguosArray.py
@@ -10,20 +10,20 @@ class Solution:
         maxLength = 0
         hashMap = {0: 0}
 
-
         for index, num in enumerate(nums, 1):
 
-            #if num 0 we will add -1 to the count else we will add + 1 to the count
+            # if num 0 we will add -1 to the count else we will add + 1 to the count
             if num == 0:
                 count -= 1
             else:
                 count += 1
 
-            #if count == 0 we will update maxlength,  with either the current max or current position - hashmap current count value which is greater.
+            # if count == 0 we will update maxlength,  with either the current max or current position - hashmap
+            # current count value which is greater.
             if count in hashMap:
                 maxLength = max(maxLength, index - hashMap[count])
             else:
-                #else: we will add that poistion to the hashMap count
+                # else: we will add that position to the hashMap count
                 hashMap[count] = index
 
         return maxLength
