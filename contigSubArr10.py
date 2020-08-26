e1f4f0d269b59e04b5de93c2a4658b17cf829761
vatsaashwin <aashwinvats@GMAIL.com>
2020-08-26 13:37:34 -0700

    Completed Hashing-2

--- contigSubArr10.py
+++ contigSubArr10.py
@@ -1,7 +1,39 @@
-# // Time Complexity :
-# // Space Complexity :
-# // Did this code successfully run on Leetcode :
-# // Any problem you faced while coding this :
+# // Time Complexity : O(n)
+# // Space Complexity : O(n)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
 
-
-# // Your code here along with comments explaining your approach
+# // Your code here along with comments explaining your approach:
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        
+        # To keep track of a cumulative sum in 'total' and of the maximum length in 'length'         
+        total = 0
+        length = 0
+        
+        # creating hashmap to track sum
+        # In the hashmap here, we'll be storing the sum as key and index of first occurance of that sum as it's value
+        hMap = {}
+        
+        # traverse through each element in given array
+        for i in range(len(nums)):
+        
+            # when a zero is encountered, add -1; else, add 1 and maintain a cumulative total
+            if nums[i] == 0:
+                total -= 1
+            else:
+                total += 1
+                
+            # If a zero is encountered, directly return the full length of array till that index.
+            # Meaning we have nullified the sum and till this point we have equal 1s and 0s
+            if total == 0:
+                length = i+1
+            
+            # If the sum is not in hashmap, enter key and its value/index, else compare the length with the difference of array from current index to the first encountered index for that sum and update length with the higher value. 
+            if total not in hMap:
+                hMap[total] = i
+            else:
+                length = max(length, i-hMap.get(total))
+        
+        # return the tracked length
+        return length
--- subArrSumEqualsK.py
+++ subArrSumEqualsK.py
@@ -1,7 +1,43 @@
-# // Time Complexity :
-# // Space Complexity :
-# // Did this code successfully run on Leetcode :
-# // Any problem you faced while coding this :
+# // Time Complexity : O(n)
+# // Space Complexity : O(n)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
 
 
 # // Your code here along with comments explaining your approach
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        # To keep track of cumulative sum-> total ; to keep track of total number of subarrays->cnt
+        total = 0
+        cnt = 0
+        
+        # create a hash map as we need to track the sum and #of subarrays as key, value pairs
+        hMap = {}
+        
+        # assigning our first case where empty array of sum 0 will be of length 1
+        hMap[0] = 1
+
+        # start traversing through the array
+        for i in range(len(nums)):
+            # add the cumulative sum
+            total += nums[i]
+            # maintain the compliment 
+            compli = total - k
+
+            # if this compliment is present in our hashmap, add the subsequent count from the hashmap
+            if compli in hMap:
+                cnt += hMap.get(compli)
+                
+            # if sum is not added to hashmap, add it and assign the value as 1 
+            # if already added, increase the value of sum by 1, meaning another subarray that sums upto the key was found
+            if total not in hMap:
+                hMap[total] = 1
+            else:
+                hMap[total] = hMap.get(total)+1
+        
+        # return the tracked number of subarrays
+        return cnt
+        
+        
