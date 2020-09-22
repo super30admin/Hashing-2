06c3a1aa9a19992ed3a00dba85e18b97342138af
Piyush Kurkure <piyushk10@gmail.com>
2020-09-21 23:39:02 -0700

    completed Hashing-2

---
+++ longest_palindrome.py
@@ -0,0 +1,19 @@
+# Time Complexity : O(N)
+# Space Complexity : O(N)
+
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ss = set()
+        for letter in s:
+            if letter not in ss:
+                ss.add(letter)
+            else:
+                ss.remove(letter)
+        if len(ss) != 0:
+            return len(s) - len(ss) + 1
+        else:
+            return len(s)
