555041c9efac8120e219a42d1b7bb5c3d5b0c8cc
jananduri <jaisurya.nanduri@copart.com>
2020-09-22 20:29:06 -0500

    Finished first problem

--- subarrayK.py
+++ subarrayK.py
@@ -4,10 +4,11 @@
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : no
 """
+import collections
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        m = defaultdict(int)
+        m = collections.defaultdict(int)
         m[0] = 1
         count, _sum = 0, 0
         for i in range(len(nums)):
