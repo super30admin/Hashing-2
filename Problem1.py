8b7272a1f53fba31c0e85d875fe854bff7a4ca7d
siddhesh25 <siddheshyadav1@gmail.com>
2020-09-23 19:47:53 -0400

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,13 +1,21 @@
-# Time Complexity: O(N^3)
+# Time Complexity: O(N)
 # Space Complexity: O(1)
-# Leetcode all test cases passed: No, 58/81 passed, TLE
-# Any difficulties: Yes, couldn't find an optimal solution. Tried sliding window but couldn't do it.
+# Leetcode all test cases passed: Yes
+# Any difficulties: No
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        sum_ = 0
         count = 0
-        for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                if sum(nums[i:j+1]) == k:
-                    count += 1
+        dic = {}
+        for i in nums:
+            sum_ += i
+            if sum_ == k:
+                count += 1                  
+            if sum_ - k in dic:             
+                count += dic[sum_ - k]
+            if sum_ not in dic:
+                dic[sum_] = 1
+            else:
+                dic[sum_] += 1
         return count
