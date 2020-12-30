6630394350caee8e313c104aab01cb3845523e36
abhisheksharma0904 <76174342+abhisheksharma0904@users.noreply.github.com>
2020-12-30 20:33:41 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,29 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+class Solution {
+    public int longestPalindrome(String s) {
+      //check if input string is null or empty, if yes - 
+        if(s == null || s.length() == 0) return 0;
+        HashSet<Character> set = new HashSet<>();
+        int count = 0;
+      //iterate till end of input string
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+          // if character matches inside the Hashset(pair formation), remove the paired value from hashset and increase count by 2 for each pair formation.
+            if(set.contains(c)){
+                count += 2; set.remove(c);
+            }
+          // else add the char in the hashset.
+            else{
+                set.add(c);
+            }
+        }
+       // if hashset is not empty, increase it by one and return because we initialized it with 0.
+        if(!set.isEmpty()) count++;
+        return count;
+    }
+}
 
 // Your code here along with comments explaining your approach
