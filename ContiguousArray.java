7344a86118b06ce1824c562235964d88af8ec5a9
narishtdhyani <narisht.dhyani@sjsu.edu>
2020-12-31 21:54:12 -0800

    Completed Hashing 2

--- ContiguousArray.java
+++ ContiguousArray.java
@@ -1,25 +1,80 @@
-// Time Complexity : O(n^2)- Brute force, n is the size of array
-// Space Complexity : O(1)-constant space
-// Did this code successfully run on Leetcode : No, Got TLE error
-// Any problem you faced while coding this : Can't think about the optimal Solution
+//Q Contiguous Array
+// Time Complexity : O(n) , n is the size of array
+// Space Complexity : O(n)-if running sum is ditinct
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 
 
 // Your code here along with comments explaining your approach
-/*
-just traversing whole array for each element and checking whether zerosCount and onesCount is equal or not.
+/* As we have to calculate max length of contiguous subarrays, so store running sum with the index when first time unique running sum appears
+Using Running Sum Pattern.Steps
+      1) Create a map for storing the running sum and the index
+      2) Initialise the map with 0 as key and -1 as value////suppose consider case [1,0] or [0,1]
+      3) Iterate over array and Calculate the running sum, by subracting running sum with 1 if nums[i]==0 and adding 1, if nums[i]==1
+      4) Check whether running sum exists in the map , if it exists then just calculate difference between current index and the running sum index. If it is greater than the max length, just update it.
+      6) If map doesn't contain the running sum, just add it with its index;
 */
 
-
+import java.util.*;
 class Solution18 {
     public int findMaxLength(int[] nums) {
-        
-        //TC -O(n^2)
-        //SC -O(1);
-        //Error : TLE
         
         if(nums==null || nums.length==0){
             return 0;
         }
+
+
+         /** Optimised using Running Sum Pattern   */
+           
+          //Storing Running Sum & Index
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);//suppose consider case [1,0]
+        
+        int runningSum = 0;
+        int maxLen = Integer.MIN_VALUE;
+        int startIndex=0;
+        int endIndex  =0;
+        
+         for(int i=0;i<nums.length;i++){
+        
+             runningSum += nums[i]==0 ? -1 : +1;
+             
+             /*if(nums[i]==1){
+                 runningSum += 1;
+             }else if(nums[i]==0){
+                 runningSum = runningSum - 1;
+             }*/
+             
+             if(map.containsKey(runningSum)){
+                 //maxLen = Math.max(maxLen,i-map.get(runningSum));
+                 int len = i-map.get(runningSum);
+                 if(maxLen<len){
+                     maxLen = len;
+                     startIndex = map.get(runningSum)+1;
+                     endIndex   = i;
+                 }
+                 
+             }else{
+                map.put(runningSum,i);    
+             }
+             
+         }
+        
+        System.out.println("Start Index = "+startIndex+" End Index ="+ endIndex);
+        
+         return maxLen==Integer.MIN_VALUE ? 0:maxLen;//Needs to be checked if only 1s or zeros are  there then initial maxLen will not be updated*/
+
+
+
+         /**        */
+        
+        /* Brute Force 
+        //TC -O(n^2)
+        //SC -O(1);
+        //Error : TLE
+        
+       
         
         int maxLen = Integer.MIN_VALUE;
         int zeroCount = 0;
@@ -43,5 +98,7 @@ class Solution18 {
         }
       
         return maxLen==Integer.MIN_VALUE ? 0:maxLen;//Needs to be checked if only 1s or zeros are there then initial maxLen will not be updated and will return Integer.MIN_LEN as max length.But I have to return 0
+
+        */
     }
 }
--- SubrrayEqualsSumK.java
+++ SubrrayEqualsSumK.java
@@ -1,14 +1,22 @@
-// Time Complexity : O(n^2)- Brute force, n is the size of array
-// Space Complexity : O(1)-constant space
+//Q - Subarray Sum Equals K
+// Time Complexity : O(n) , n is the size of array
+// Space Complexity : O(n)-if running sum is distinct
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : Can't think about the optimal Solution
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
-/*
-just traversing whole array for each element and checking whether sum of each element == k or not.
+/* As we have to calculate total number of contiguous subarrays, so store running sum with the count how many time running sum appears
+Using Running Sum Pattern.Steps
+      1) Create a map for storing the running sum and the count
+      2) Initialise the map with 0 as key and 1 as value
+      3) Iterate over array and Calculate the running sum
+      4) While iterartion, find the compliment of running sum and the k. 
+      5) Check whether compliment exists in the map , if it exists then just add its values to the global count
+      6) Store the running sum in the map with a default value 1, if running sum already exists just increment the running sum count in the map
 */
 
+import java.util.*;
 
 class Solution17 {
     public int subarraySum(int[] nums, int k) {
@@ -17,7 +25,32 @@ class Solution17 {
             return 0;
         }
         
-        //O(n^2)
+       /** Optimised using Running Sum Pattern   */
+
+       int count =0;
+       Map<Integer,Integer> map = new HashMap<>();
+       map.put(0,1);//Assuming that rsum is 0 and count is 1
+       int rSum =0;
+       
+       for(int i=0;i<nums.length;i++){
+           
+           rSum += nums[i];
+           
+           int compliment = rSum-k;
+           
+           if(map.containsKey(compliment)){
+               count += map.get(compliment);
+           }
+           map.put(rSum,map.getOrDefault(rSum,0)+1);
+       }
+       
+       
+       return count;
+       
+       /** */
+       /* 
+        Brute Force
+       //O(n^2)
         int count = 0;
         int sum;
         for(int i=0;i<nums.length;i++){
@@ -32,6 +65,6 @@ class Solution17 {
             }
         }
         
-        return count;
+        return count;*/
     }
 }
