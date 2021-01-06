c257954c808ba9a8298f66e980d3035f9f5dfa08
Saphal Thammu <saphalthammu@Saphals-MacBook-Pro.local>
2021-01-06 14:03:03 -0700

    Completed Contiguous Array

--- Problem2_ContiguousArray.cpp
+++ Problem2_ContiguousArray.cpp
@@ -6,12 +6,24 @@
 
 // Your code here along with comments explaining your approach
 
+
+#include<vector>
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<cmath>
+#include<time.h>
+#include<iostream>
+
+using namespace std;       
+
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         
         if (nums.empty()) return 0;
         int gmax=0;
+        int start =0;
+        int end=0;
         
         unordered_map<int,int> map;
         
@@ -21,13 +33,19 @@ public:
         for(int i=0;i<nums.size();i++){
             rSum= nums[i]==0?rSum-1 :rSum+1;
             
-            if(map.count(rSum))
-                gmax= max(gmax,i - map.at(rSum));
+            if(map.count(rSum)){
+                if(gmax<i-map.at(rSum)){
+                    gmax=i-map.at(rSum);
+                    end=i;start=map.at(rSum)+1;
+                }
+            }
+            //    gmax= max(gmax,i - map.at(rSum));
             else{
                 map.insert(make_pair(rSum,i));
             }
             
         }
+        std::cout<<start<<endl<<end<<endl;
         return gmax;      
         
         
@@ -39,3 +57,12 @@ public:
         
     }
 };
+
+int main(){
+    Solution a;
+    vector<int> nums={0,1,0,0,0,1,1,1,0};
+    //int target=2;
+    int b = a.findMaxLength(nums);
+    // for (int x : b) 
+    //     cout << x << " "; 
+}
