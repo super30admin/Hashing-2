41e88e55aea0459d72e63efef79360bb0af7ea56
Nilesh Deokar <76528900+nilesh-525@users.noreply.github.com>
2021-03-08 21:46:22 -0500

    Update ContiguousArray.java

--- ContiguousArray.java
+++ ContiguousArray.java
@@ -6,36 +6,37 @@ import java.util.*;
 // Any problem you faced while coding this : Was new to Running sum technique
 
 class ContiguousArray {
+    
+    /*
+        Approach :
+        arr:  [0, 1, 1, 1, 0, 1, 1, 1]
+        sum:  [-1,0, 1, 2, 1, 2, 3, 4]
+    */
+    public int findMaxLength(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int sum = 0;
+        int max = 0;
+        map.put(0,-1); // edge case [0,1] or [1,0]
+    
 
-    public static void main(String[] args){
-
-    }
-
-    private static int findMaxLength(int[] nums) {
-      if(nums == null || nums.length == 0) return 0;
-
-      int maxLength = 0;
-      HashMap<Integer, Integer> hMap = new HashMap<Integer,Integer>();
-      int rSum = 0;
-
-      for(int i = 0; i < nums.length; i++){
-        if(nums[i] == 0){
-          rSum -= 1;
-        }else{
-          rSum += 1;
+        for(int i = 0; i < nums.length; i++){            
+            if(nums[i] == 0){
+                sum -= 1;
+            }else{
+                sum += 1;
+            }        
+            
+            if(map.containsKey(sum)){
+                max = Math.max(max, i - map.get(sum));
+            }else{
+                map.put(sum, i);
+            }
         }
-        if(hMap.containsKey(rSum)){
-          maxLength = Math.max(maxLength, i - hMap.get(rSum));
-        }else{
-          hMap.put(rSum, i);
-        }
-
-
-
-      }
-
-
-
-      return maxLength;
+       
+        
+        return max;
     }
 }
