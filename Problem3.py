d0448176b3fd200337934bfe048f8c85a6ea2261
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-06-01 11:17:58 +0530

    Completed Problem3

--- Problem3.py
+++ Problem3.py
@@ -1,4 +1,29 @@
-# Time Complexity : 
-# Space Complexity : 
-# Did this code successfully run on Leetcode : 
-# Any problem you faced while coding this : 
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+#saving the occurences of each character and checking for even number of
+# occurences and saving the one extra odd occurence to put it in middle
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        a = {}
+        for i in s:
+            if i not in a:
+                a[i] = 1
+            else:
+                a[i] += 1
+        palLen = 0
+        cont = False
+        for i in a:
+            if a[i] % 2 == 1:
+                cont = True
+                palLen += (a[i]-1)
+            else:
+                palLen += a[i]
+        if cont == True:
+            return palLen + 1
+        return palLen
+        
