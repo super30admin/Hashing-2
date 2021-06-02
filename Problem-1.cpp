ff2c5ad505f7e5d9ab3ef6534dff7d0d8444d7ee
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-06-01 21:00:44 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -9,17 +9,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int ret = 0;
+        int ans = 0;
         int sum = 0;
         unordered_map<int, int> map;
         map[0] = 1;
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
             if (map.find(sum - k) != map.end()) {
-                ret += map[sum - k];
+                ans += map[sum - k];
             }
             map[sum]++;
         }
-        return ret;
+        return ans;
     }
 };
