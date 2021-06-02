9b99cc4bd7e7f63770ee74e36dc78af82a5c51be
Sumit Chavan <chavansumit13@gmail.com>
2021-06-02 05:42:48 +0530

    added another approach for subarraysumequalsk

--- src/src/SubArraySumEqualsk.java
+++ src/src/SubArraySumEqualsk.java
@@ -1,13 +1,19 @@
-//TC : O(n^2) where n is the length of the string given
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SubArraySumEqualsk {
+
+    //TC : O(n^2) where n is the length of the string given
 //SC : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
-
-//the approach taken is that every subarray is considered and the sum is calculated on the go.
+    //the approach taken is that every subarray is considered and the sum is calculated on the go.
 //if the sum is equal to k, then increase the counter.
-public class SubArraySumEqualsk {
-
     public int subarraySum(int[] nums, int k) {
         int count = 0;
         for (int start = 0; start < nums.length; start++) {
@@ -18,6 +24,40 @@ public class SubArraySumEqualsk {
                     count++;
             }
         }
+        return count;
+    }
+
+    //TC : O(n) where n is the length of the string given
+//SC : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+    //The main idea of this approach is that sum[i,j] = sum[0,j] - sum[0,i-1]. Therefore, as we traverse the array,
+    //we can store the sum till that index and check whether the currentSum-k is present in the map. If yes then increment count by the value present
+    //else just add the entry in the map.
+    public int subarraySum_approachTwo(int[] nums, int k) {
+        int sum = 0, count = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+
+        //we add (0,1) for the case where sum - k = 0
+        preSum.put(0, 1);
+
+
+        for (int num : nums) {
+            sum += num;
+
+            //sum[i,j] = sum[0,j] - sum[0,i-1]
+            //k = sum - hashMapkey . Therefore hashMapKey = sum - k
+            //hence we check for the count of such sums (sum-k) and increase count by that
+            if (preSum.containsKey(sum - k)) {
+                count += preSum.get(sum - k);
+            }
+
+            //increment count for the same same or add a new entry
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+
         return count;
     }
 }
