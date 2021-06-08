d9033775dc240c188baab9f9cfa6a3512079e3e7
Zaiba Fathima <zaibafathima999@gmail.com>
2021-06-07 18:29:41 -0700

    Completed Hashing-2

---
+++ LongestPalindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+
+        int count = 0;
+
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (set.contains(c)) {
+                set.remove(c);
+                count += 2;
+            } else {
+                set.add(c);
+            }
+        }
+
+        if (!set.isEmpty())
+            count++;
+        return count;
+
+    }
+}
+
+/*
+ * Time complexity : O(n) Space complexity : O(n)
+ */
