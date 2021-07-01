9722b08e14fe52d11ecef4a37ae753a30ee99932
Neha Poonia <69773692+NehaPoonia@users.noreply.github.com>
2021-07-01 11:05:17 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,4 +1,4 @@
-#Brute force
+'''#Brute force
 #time comp- O(n**2)
 #space comp-O(1)
 class Solution:
@@ -10,4 +10,25 @@ class Solution:
                 curr_sum=curr_sum+nums[j]
                 if curr_sum==k:
                     res=res+1 # to take count so far
-        return res
+        return res'''
+#time comp- O(n) #only traversing once 
+#space comp-O(k) length of key value pairs
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        r_sum=0
+        count=0
+        d={0:1}
+        for i in range(0,len(nums)):
+            r_sum=r_sum+nums[i]
+            if r_sum-k in d:
+                count=count+d[r_sum-k]
+            if r_sum not in d:
+                d[r_sum]=1
+            else:
+                d[r_sum]=d[r_sum]+1
+            #print(d)
+        return count
+            
+            
+            
+            
