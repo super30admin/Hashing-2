135800315830b17be06d1de22022e27c62e1acf2
msahithi29 <50932067+msahithi29@users.noreply.github.com>
2021-07-28 13:29:38 -0700

    Completed Problem 2 Hashing-2

---
+++ ContiguousArray.java
@@ -0,0 +1,27 @@
+//525. Contiguous Array - Passed all test cases
+/*
+TC: O(n)
+SC: O(n)
+Method: Using running sum and a hashmap
+
+*/
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+       Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==0)
+                count = count -1;
+            else
+                count = count + 1;
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+        return maxlen; 
+    }
+}
