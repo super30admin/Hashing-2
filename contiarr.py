1f7fcb026032ae26d1ecd5ec4d8fe682ef104f74
Smriti0311 <smritigrao@gmail.com>
2021-08-24 14:23:39 -0400

    Completed contiguous subarray with equal 0 and 1

--- contiarr.py
+++ contiarr.py
@@ -1,15 +1,28 @@
-# Time Complexity : O(N) 
-# Space Complexity : O(1) 
-# Did this code successfully run on Leetcode : Partially, 50 % of the test cases
-# Any problem you faced while coding this : I didn't get the logic fully correct
+# Time Complexity : O(N) due to the single pass for loop
+# Space Complexity : O(N) for the dictionary 
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : No
 
 
 
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        curr_sum = nums[0]
-        max_sum = nums[0]
-        for i in range(1, len(nums)):
-            curr_sum = max(nums[i] + curr_sum, nums[i])
-            max_sum = max(curr_sum, max_sum)
-        return max_sum * 2
+        if len(nums) == 0:
+            return 0
+        count = 0
+        mydict = {}
+        res = 0
+        mydict[0] = -1
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count -= 1
+            else:
+                count += 1
+            if count in mydict:
+                res = max(res, (i - mydict[count]))
+            else:
+                mydict[count] = i
+        return res
+            
+        
+        
