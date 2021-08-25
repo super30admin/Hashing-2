80687dfff03d6317d543641cd24a86797fca67a3
ChinweCodes <njokuchinwendum@gmail.com>
2021-08-25 17:54:51 -0600

    Hashing-2 completed

--- Sample.java
+++ Sample.java
@@ -1,4 +1,4 @@
-// Time Complexity :
+// Time Complexity :O()
 // Space Complexity :O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : I don't properly understand this problem
@@ -24,3 +24,60 @@ class Solution {
     }
 }
 
+
+// Time Complexity :O(1)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : I don't properly understand this problem
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap();
+        map.put(0, -1);
+        
+        int maximumLength = 0;
+        int count = 0;
+        
+        //loops through the array linearly and increment the counter if it is 1 and decrement the counter if it is zero.
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                count += -1;
+            }
+            else{
+                count += 1;
+            }
+            
+            if(map.containsKey(count)) {//checks if we have see the count before and if we have,
+                maximumLength = Math.max(maximumLength, i-map.get(count)); //we check against the max
+            }
+            else{
+                map.put(count, i);
+            }
+        }
+        return maximumLength;//The return the maximum length of the sub-array.
+        
+    }
+}
+
+
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : I don't properly understand this problem
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] charCount = new int[128]; //An array that keeps count of the number of occurrencies of a character
+        for(char c : s.toCharArray()){//Loop through all the characters in the string
+            charCount[c]++; //Access the index of the string by passing in c which is the character that would be converted to the ASCII index in this array.
+        }
+        
+        int result = 0 //Initialize the result 
+        for(Integer characterCount: charCount) {//Loop through each integer in the character count array.
+            result += characterCount / 2 * 2; //This integer division adds on any pair of integer that is divisible by 2
+            if(result % 2 == 0 && characterCount % 2 == 1){
+                result += 1;
+            }
+        }
+        return result;
+    }
+}
