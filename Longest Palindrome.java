aa2714862a1fb508468930b9931e466d3b27a0f7
Rishi Raj Gupta <90346515+rishirg30@users.noreply.github.com>
2021-09-27 22:21:13 -0400

    Add files via upload

---
+++ Longest Palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int []char_counts = new int [128];
+        for(char c: s.toCharArray()){
+            char_counts[c]++;
+            
+        }
+        int result =0;
+        for (Integer char_count: char_counts){
+        result +=char_count/2*2;
+            if (result%2==0 && char_count %2==1){
+                result +=1;
+            }
+            
+        }
+        return result;   
+    }
+}
