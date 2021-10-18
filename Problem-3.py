404ac9b51c0ee40f296767d185100c3978fdf495
Adhish Shrivastava <adhish70@gmail.com>
2021-10-18 09:38:38 -0400

    Problem-3 done

--- Problem-3.py
+++ Problem-3.py
@@ -1,9 +1,24 @@
 # 409. Longest Palindrome
 # https://leetcode.com/problems/longest-palindrome/
 
-# Time Complexiety: 
-# Space Complexiety: 
+# Time Complexiety: O(n)
+# Space Complexiety: O(n)
 
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        pass
+        temp_set = set()
+        count = 0
+
+        for i in s:
+            if i in temp_set:
+                temp_set.remove(i)
+                count += 2
+            else:
+                temp_set.add(i)
+        
+        return count if not temp_set else count+1
+
+obj = Solution()
+print(obj.longestPalindrome('abccccdd'))
+print(obj.longestPalindrome('a'))
+print(obj.longestPalindrome('bb'))
