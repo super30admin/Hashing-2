f26e7f6d71372e6ed458d7c72f26e5709d3633d5
AnjaliJain-17 <anjali.wie@gmail.com>
2021-12-01 23:53:17 -0800

    hashing 2

---
+++ Problem3.java
@@ -0,0 +1,18 @@
+public class Problem3 {
+    public int longestPalindrome(String s) {
+        //if you encounter an element for the first time
+        //put it in set, if you encounter it again and increment the countby2 & remove         //the element.
+        if(s == null || s.length() == 0){ return 0; }
+        int count=0;
+        Set<Character> set = new HashSet<>();
+        for(char c : s.toCharArray()){
+            if(set.contains(c)){
+                count+=2;
+                set.remove(c);
+            }else{
+                set.add(c);
+        }
+        }
+        return (set.size()==0)?count:count+1;
+}
+}
