5d9c367ac10c6a5d1d13a0d891e5d5e162da1b4d
Gauri <sarpotdar.g@husky.neu.edu>
2021-12-12 21:48:06 -0500

    All added

---
+++ ContiguousArray.java
@@ -0,0 +1,27 @@
+// Time Complexity :O(N)
+// Space Complexity :O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :No
+class Solution {
+    public int findMaxLength(int[] nums) {
+        if(nums == null || nums.length ==0)return 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int count =0,max =0;
+        for(int i =0; i<nums.length;i++){
+            if(nums[i]==0){
+                count --;
+            }
+            else{
+                count ++;
+            }
+            if(map.containsKey(count)){
+                max = Math.max(max, i-map.get(count));
+            }
+            else{
+                map.put(count,i);
+            }
+        }
+        return max;
+    }
+}
