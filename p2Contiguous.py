3604a64f0161ca0d9ad5022e50d42ac645e5ee53
LiberoPat <patrick.trias@gmail.com>
2021-12-28 16:55:09 -0800

    complete, some expanations for each prob

---
+++ p2Contiguous.py
@@ -0,0 +1,42 @@
+"""
+
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+
+To find the maximum length, we need a dict to store the value of count (as the key) and
+ its associated index (as the value). We only need to save a count value and its index 
+ at the first time, when the same count values appear again, we use the new index subtracting 
+ the old index to calculate the length of a subarray. A variable max_length is used to to keep
+  track of the current maximum length.
+
+Complexity
+Time: O(N)
+Space: O(1)
+
+
+"""
+
+class Solution(object):
+    def findMaxLength(self, nums):
+        count = 0
+        max_length=0
+        table = {0: 0}
+        for index, num in enumerate(nums, 1):
+            if num == 0:
+                count -= 1
+            else:
+                count += 1
+            
+            if count in table:
+                max_length = max(max_length, index - table[count])
+            else:
+                table[count] = index
+        
+        return max_length
---
+++ p3Palindrome.py
@@ -0,0 +1,52 @@
+ """"
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+
+Example 1:
+
+Input: s = "abccccdd"
+Output: 7
+Explanation:
+One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+
+Note) The set 'ss' defined above stores the count of unique letters that have odd counts. 
+ So, for an array [b, e, d, d, b, b] it would store b and e as they have odd counts.
+
+Why?
+The main intention is to count one for all odd count letters 
+    after accounting for the even pairs in them if any. For example in [b, e, d, d, b, b]
+    we know that for b and e only one count should be taken after accounting for the pair 
+    'bb' for 'b'. This is because it doesn't make any difference in the final answer whether
+    you have b or e in the middle('bdedb' or 'bdbdb' returns 5). Hope this clears some
+    confusion.
+
+ii) Finally, if there are no odd count letters present we want to count all even values 
+for the counts(which is basically the length of the given array) as the final answer. 
+Otherwise, we would need to adjust for the extra odd counts that we are doing when we are 
+counting all the values by removing the count of all distinct odd letters and then counting 
+1 for them.
+ 
+Complexity:
+Time:O(n)
+Space: O(1)
+
+
+ """  
+   
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        hash = set()
+        for c in s:
+            if c not in hash:
+                hash.add(c)
+            else:
+                hash.remove(c)
+        # len(hash) is the number of the odd letters
+        return len(s) - len(hash) + 1 if len(hash) > 0 else len(s)
