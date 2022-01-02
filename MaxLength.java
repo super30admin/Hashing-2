a810ed4a58bcc4a3b9601b5b19368d2aa4a8310b
chaitra <chaitra.kallianpur@hpe.com>
2022-01-02 15:47:39 -0500

    Hashing-2 Java Implementation

---
+++ MaxLength.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+// TC O(N) SC O(N)
+public class MaxLength {
+    public int findMaxLength(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        HashMap<Integer, Integer> len = new HashMap<>(); // rsum:idx
+        len.put(0, -1);
+        int max = 0;
+        int rSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                rSum = rSum - 1;
+            } else {
+                rSum = rSum + 1;
+            }
+            if (len.containsKey(rSum)) {
+                max = Math.max(max, i - len.get(rSum));
+            } else {
+                len.put(rSum, i);
+            }
+        }
+        return max;
+    }
+}
