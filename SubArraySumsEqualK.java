be3ac4959e036f9091b145dc7ef75c103c54f9c3
Mohit Parkhi <31873157+Mohit1215@users.noreply.github.com>
2022-01-01 19:29:44 -0500

    Improved sub array sum equals k

--- SubArraySumsEqualK.java
+++ SubArraySumsEqualK.java
@@ -29,3 +29,33 @@ class SubArraySumsEqualK
      return count;
    }
 }
+
+
+// Solution with improved time complexity : O(N) using prefix sum and HashMap
+// time complexity : O(N)
+// space complexity : O(N), worst case it was increasing sum
+//                   and we had to include keys for each sum.
+public int subarraySum(int[] nums, int k)
+{
+  Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+// extra case added to make sure the first time k sum appears we don't miss it.
+  map.put(0, 1);
+  int count = 0;
+  int sum = 0;
+
+  for(int i = 0; i < nums.length; i++)
+  {
+      sum += nums[i];
+
+      map.putIfAbsent(sum,0);
+
+      if(map.containsKey(sum - k)){
+          count += map.get(sum - k);
+      }
+
+      map.put(sum, map.get(sum) + 1);
+  }
+
+  return count;
+}
