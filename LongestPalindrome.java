9ddb1433cf1236e0b2afa527e4a0e792e52e6b7b
akashamin01 <aakashjamin1239@gmail.com>
2022-01-07 15:55:35 -0800

    Completed Hashing 2

---
+++ LongestPalindrome.java
@@ -0,0 +1,19 @@
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int result = 0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(set.contains(c)){
+                result+=2;
+                set.remove(c);
+            }else{
+                set.add(c);
+            }
+        }
+        if(!set.isEmpty()) result++;
+        return result;
+    }
+}
