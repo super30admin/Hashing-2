97e5f1bc0e64dc3d32c5d2ec2a2905d6d46826d9
sanyami30 <61071285+sanyami30@users.noreply.github.com>
2022-01-12 14:10:35 -0800

    Time: 8 ms (18.01%), Space: 39.3 MB (10.11%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1) because it will not be more than 26
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
 class Solution {
     public int longestPalindrome(String s) {
         HashSet<Character> set = new HashSet<>();
