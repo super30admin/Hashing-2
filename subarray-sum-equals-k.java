f9235182486c1becefb2ab362b2f0d3538235931
Viraj <59378815+VirajDeshmukh1@users.noreply.github.com>
2022-04-13 19:21:32 -0700

    Contiguous array, longest palindrome, subarray sum equals k - Solutions added

---
+++ subarray-sum-equals-k.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        if(nums==null || nums.length==0){
+            return 0;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int rsum = 0;
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            rsum += nums[i];
+            int compliment = rsum - k;
+            if(map.containsKey(compliment)){
+                count += map.get(compliment);
+            }
+            if(!map.containsKey(rsum)){
+                map.put(rsum, 0);
+            }
+            map.put(rsum, map.get(rsum) + 1);
+        }
+        return count;
+    }
+}
+
+//Time Complexity : O (n)
+//Space Complecity : O(n)
