e058d975d5b6d4d7185f07e99020760a1ccef00e
abhi1288 <abhishekmantri2010@gmail.com>
2022-05-04 22:10:24 -0700

    adding running sum solution as well

--- ContiguousArrayLengthBinaryLeetcode509.java
+++ ContiguousArrayLengthBinaryLeetcode509.java
@@ -1,7 +1,7 @@
 //Time Complexity O(N^2)
 //Space Complexity O(1)
 //Leetcode: Yes, but the brute force solutin timesout
-
+import java.util.HashMap;
 public class ContiguousArrayLengthBinaryLeetcode509 {
     public int findMaxLength(int[] nums) {
         int end = nums.length;
@@ -22,5 +22,27 @@ public class ContiguousArrayLengthBinaryLeetcode509 {
             }
         }
         return max_len;
+    }
+
+   //Time Complexity O(N)
+    //Space Complexity O(N)
+    //Leetcode : yes
+    public int findMaxLength1(int[] nums) {
+        int n = nums.length, max = 0;
+        HashMap<Integer, Integer> index = new HashMap<>();
+        index.put(0, -1);
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) sum--;
+            else sum++;
+
+            if (index.containsKey(sum)) {
+                max = Math.max(max, i - index.get(sum));
+            } else {
+                index.put(sum, i);
+            }
+        }
+        return max;
     }
 }
