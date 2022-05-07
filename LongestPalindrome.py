4395fc6ec0b5ad1054f97adda386561798bfd7a5
Charishma090619999 <93498653+Charishma090619999@users.noreply.github.com>
2022-05-06 20:00:48 -0700

    commited 2 problems

---
+++ LongestPalindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        dict1,res,flag={},0,0
+        for i in s:
+            if i not in dict1:
+                dict1[i]=1
+            else:
+                dict1[i]+=1
+        for i in dict1:
+            if(dict1[i]>=2 and dict1[i]%2==0):
+                res+=dict1[i]
+            elif(dict1[i]%2!=0):
+                res+=dict1[i]-1
+                flag=1
+        if flag==1:
+            return res+1
+        else:
+            return res
+        
