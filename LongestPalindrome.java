2ab703a0cc0cc656e29ea11ab1b15fd0f8721566
arshmohd <irfan.arshad4@gmail.com>
2022-05-10 15:45:09 +0530

    Implemented the Longest Palindrome logic using count and simple iteration and updated the Time and Space complexity

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,3 +1,25 @@
-public class LongestPalindrome {
+import java.util.HashMap;
 
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
+public class LongestPalindrome {
+  public static int longestPalindrome(String s) {
+    int[] count = new int[256];
+    for (char character : s.toCharArray())
+      count[character]++;
+    int finalLength = 0;
+    for (int val : count) {
+      finalLength += val / 2 * 2;
+      if (finalLength % 2 == 0 && val % 2 == 1)
+        finalLength++;
+    }
+    return finalLength;
+  }
+
+  public static void main(String[] args) {
+    String s = "abccccdd";
+    System.out.println(longestPalindrome(s));
+  }
 }
