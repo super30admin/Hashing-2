ebc9672fe943cff0a8a8a17a173063e4c70ff0f4
arshmohd <irfan.arshad4@gmail.com>
2022-05-10 15:37:24 +0530

    Implemented the algo for SubArraySum equals to K using HashMap

---
+++ SubArraySumK.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class SubArraySumK {
+  public static int SubArrSum(int[] nums, int k) {
+    int count = 0, sum = 0;
+    HashMap<Integer, Integer> hmap = new HashMap<>();
+    hmap.put(0, 1);
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      if (hmap.containsKey(sum - k))
+        count += hmap.get(sum - k);
+      hmap.put(sum, hmap.getOrDefault(sum, 0) + 1);
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3 };
+    int k = 3;
+    System.out.println(SubArrSum(arr, k));
+  }
+}
