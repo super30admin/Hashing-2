2329bdaa633eea41d0c5ae3c464df08dd3b2fa1e
aashutoshtaikar <aashutoshtaikar@gmail.com>
2022-05-11 04:36:10 -0400

    problem link

--- Problem17.java
+++ Problem17.java
@@ -1,15 +1,18 @@
 import java.util.HashMap;
 
 /**
-*   Running sum pattern
-*   3   4   7   2   -3  1   4   2   0   1
-*   3   7   14  16  13  14  18  20  20  21
-*   |----------y-----------------|   current runningSum y : 20
-*   |----x-----------|               running sum happened Before : y-z = x : 20-7 = 13 
-*                    |-----z-----|   target sum : z : 7
-*
-*   if the running sum happened before then definitely the sub array(z) sum is 7 
-*
+ * https://leetcode.com/problems/subarray-sum-equals-k/
+ *   Running sum pattern
+ *   3   4   7   2   -3  1   4   2   0   1
+ *   3   7   14  16  13  14  18  20  20  21
+ *   |----------y-----------------|   current runningSum y : 20
+ *   |----x-----------|               running sum happened Before : y-z = x : 20-7 = 13 
+ *                    |-----z-----|   target sum : z : 7
+ *
+ *   if the running sum happened before then definitely the sub array(z) sum is 7 
+ *   
+ * time: O(n)
+ * space: O(n)
 */
 class Solution {
     public int subarraySum(int[] nums, int k) {
