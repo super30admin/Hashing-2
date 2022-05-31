ef92f288a75f9ed501e01446224c168486341300
Hanuman Reddy <39452135+HanumanReddy1212@users.noreply.github.com>
2022-05-31 12:46:28 -0500

    Hashing -2 done please review

---
+++ Subarray Sum Equals K.java
@@ -0,0 +1,18 @@
+//Time complexity O(n), Space complexity O(n).
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        
+        return result;
+    }
+}
