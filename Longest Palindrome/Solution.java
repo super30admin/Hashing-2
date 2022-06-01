fc2c1520ad875a43cc7e50f076bc9566f0d98af6
Vraj Himanshu Reshamdalal <30725458+vraj9845@users.noreply.github.com>
2022-06-01 17:49:27 -0400

    Create Solution.java

---
+++ Longest Palindrome/Solution.java
@@ -0,0 +1,19 @@
+// Time complexity = O(n)
+// Space complexity = O(n)
+class Solution {
+    public int longestPalindrome(String s) {
+        int count=0;
+        HashSet<Character> set = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(set.contains(c)){
+                count+=2;
+                set.remove(c);
+            } else{
+                set.add(c);
+            }
+        }
+        if(!set.isEmpty()) count++;
+        return count;
+    }
+}
