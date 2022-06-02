d2bc66b68392328e0e72fe50a6676f0feabe95bd
Siddesh2044 <60492570+Siddesh2044@users.noreply.github.com>
2022-06-01 20:34:45 -0400

    Completed contiguous-array

--- contiguous-array.java
+++ contiguous-array.java
@@ -1,1 +1,26 @@
+//Time Complexity --> O(n)--> We are traversing the array using for loop
+//Space Complexity --> O(n)
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int rSum = 0;
+        int max = 0;
+        map.put(0, -1);
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) rSum--;
+            else {
+                rSum++;
+            }
+            if(map.containsKey(rSum)) {
+            max = Math.max(max, i - map.get(rSum));
+            }else
+                map.put(rSum, i);
+        }
+        return max;
+    }
+}
+
+
 
