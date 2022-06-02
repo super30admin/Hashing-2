ce7845340c3e5faa703b4f475ec6305fd492b7c0
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-02 10:48:43 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,86 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//****LONGEST PALINDROME****
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : Y
+// Any problem you faced while coding this : N
+
+class Solution {
+    public int longestPalindrome(String s) {
+Set<Character> set= new HashSet<>();
+int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch= s.charAt(i);
+            if(set.contains(ch))
+            {
+                count=count+2;
+                set.remove(ch);
+            }
+            else
+            {
+                set.add(ch);
+            }
+        }
+        
+        if(!set.isEmpty())
+        {
+            count=count+1;
+        }
+        
+        return count;
+    }
+}
 
 
-// Your code here along with comments explaining your approach
+
+
+//****CONTIGIOUS SUBARRAY****
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : Y
+// Any problem you faced while coding this : N
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        //Hashmap of type - (rSum, Index);
+        HashMap <Integer, Integer> map =new HashMap<>();
+        map.put(0,-1);
+        int rSum=0;
+        int max=0;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0) 
+            {
+                rSum--;
+            }
+            else
+            {
+                rSum++;
+            }
+            
+            if(map.containsKey(rSum))
+            {
+                int x=i-map.get(rSum);
+                max=Math.max(max, x);
+            }
+            else
+            {
+                map.put(rSum, i);
+            }
+        }
+       return max; 
+    }
+}
+
+
+
+//****SUBARRAY SUM EQUALS K****
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : Y
+// Any problem you faced while coding this : N
+
+
+
+
