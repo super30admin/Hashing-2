074f6e16edd36359753c881170d5fd75d1e70b6c
getumangon <getumangon@gmail.com>
2022-06-02 22:36:53 +0530

    Added comments

--- findMaxLength.java
+++ findMaxLength.java
@@ -1,3 +1,8 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Did this code successfully run on Leetcode: Yes
+// Any problem you faced while coding this: No 
+
 import java.util.HashMap;
 
 public class findMaxLength {
