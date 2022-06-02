734e011e00abd3428610f6be00da4fbbcf0cdd5d
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-02 12:35:52 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -81,6 +81,35 @@ class Solution {
 // Did this code successfully run on Leetcode : Y
 // Any problem you faced while coding this : N
 
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map= new HashMap<>();
+        map.put(0,1);
+        int result=0;
+        int rSum=0;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            rSum += nums[i];
+            if(map.containsKey(rSum-k))
+            {
+                result +=map.get(rSum-k);
+            }
+            
+                if(map.containsKey(rSum))
+                {
+                    map.put(rSum, map.get(rSum)+1);
+                }
+                else
+                {
+                    map.put(rSum, 1);
+                }
+            
+        }
+        return result;
+    }
+}
+
 
 
 
