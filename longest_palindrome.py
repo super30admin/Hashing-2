f7fd4e3554f33ca73784bdeeaafbb868544f21eb
navya0405 <navya.spin@gmail.com>
2022-06-28 23:50:20 -0700

    completed longest palindrome

---
+++ longest_palindrome.py
@@ -0,0 +1,19 @@
+#time complexity: O(n)
+#space complexity: O(n)
+#approach: even number of elements are added as it is and odd count of elements are added only one in total.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hashset = set()
+        count = 0
+        for letter in s:
+            if letter in hashset:
+                count = count + 2
+                hashset.remove(letter)
+            else:
+                hashset.add(letter)
+        if len(hashset)==0:
+            return count
+        else:
+            return count+1
+        
