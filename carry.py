b851210451a5edac25c0a0a8b00d715addc0c79b
aneesh2411 <aneeshkalisapudi@gmail.com>
2022-06-29 19:29:52 -0700

    completed hashing 2

---
+++ carry.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        hashmap = dict()
+        list = []
+        max = 0
+        sum = 0
+        diff = 0
+        hashmap[0] = -1
+        for i in range(len(nums)):
+            if(nums[i]==1):
+                sum = sum + 1
+            else:
+                sum = sum -1
+            if(hashmap.get(sum) is not None):
+                diff = i - hashmap.get(sum)
+                if(diff>max):
+                    max = diff
+            else:
+                hashmap[sum] = i
+        return max
