43c41a5dd3f60790de7e3c96f2a31a14d837a3f8
Darshil Shah <darshilshah1899@gmail.com>
2022-08-16 17:38:53 -0400

    Hashing 2
    Hashing 2

---
+++ Subarray.java
@@ -0,0 +1,21 @@
+// Time Complexity : O(n)
+// Space Complexity : o(n)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
