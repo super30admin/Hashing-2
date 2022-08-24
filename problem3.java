4bcffefefa512052bc3afd96759845cc8eacc235
DSP22SCM54G <devgoel30@gmail.com>
2022-08-23 21:53:28 -0500

    Done Hashing-2 Please Review

---
+++ problem3.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+// ## Problem3 (https://leetcode.com/problems/longest-palindrome)
+
+class Solution {
+    // space O(1)
+    // time O(n)
+    public int longestPalindrome(String s) {
+        int count = 0;
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (set.contains(c)) {
+                count+=2;
+                set.remove(c);
+            } else {
+                set.add(c);
+            }
+        }
+        if (!set.isEmpty()) count++;
+        return count;
+        
+    }
+}
