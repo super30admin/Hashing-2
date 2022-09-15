ace961a8674b1ab29de505b9ee8e737145e6d3de
Lakshay Nanda <39439983+lakshaynanda@users.noreply.github.com>
2022-09-15 01:03:26 -0700

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+// TC: O(n)
+// SC: O(n)
+
+class Solution {
+    public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        HashSet<Character> set = new HashSet<>();
+        int length = 0;
+        
+        for (char c: s.toCharArray()) {
+            if(set.contains(c)) {
+                length += 2;
+                set.remove(c);
+            } else {
+                set.add(c);
+            }
+        }
+        return set.isEmpty() ? length: length + 1;
+    }
+}
