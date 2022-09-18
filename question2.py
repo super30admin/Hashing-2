c6c854cb907792f206fbb73d8444bac8a869b537
Jas Bhavesh Zaveri <Zaveri.Jas@cognizancetech.com>
2022-09-18 18:14:32 -0500

    Completed Sprint - Hashing-2

---
+++ question2.py
@@ -0,0 +1,27 @@
+#Time Complexity : O(n)
+#Space Complexity : O(n)
+#Did this code successfully run on Leetcode : Yes
+#Any problem you faced while coding this : None
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        max_length=0
+        hash={}
+        count=0
+        for i in range(len(nums)):
+            current=nums[i]
+            if current==0:
+                count-=1 # decrement our count if our current element is 0
+            else:
+                # increment our count if current element is 1
+                count+=1
+
+            if count==0:
+                # if count is 0, we have a new subarray with length+1
+                max_length=i+1
+            if count in hash:
+                max_length=max(max_length,i-hash[count]) 
+            else:
+                hash[count]=i
+        return max_length
+
