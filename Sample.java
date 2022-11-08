84a58e882eb89dbb56c43ed9d2b0f5821cfc9682
Riddhi Jaju <riddhijaju2610@gmail.com>
2022-11-07 23:01:17 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,43 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//Problem 525(Contiguous Array)
+
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :YES
+// Any problem you faced while coding this :NO
 
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map =new HashMap<>();
+        int max=0;
+        int rsum=0;
+        map.put(0,-1);
+        //Iterate over array
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+                rsum--;
+            }
+            else
+            {
+                rsum++;
+            }
+            if(map.containsKey(rsum))
+            {
+                int x=i-map.get(rsum);
+                max=Math.max(max, x);
+            }
+            else
+            {
+                map.put(rsum, i);
+            }
+        }
+        //check if map contains
+            //update max
+        //put if map doesnt contain
+     return max;   
+    }
+}
