77069e9a75f714ed307fbd8db0ff306324e1ce05
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-02-08 17:06:15 -0500

    Create LongestPalindrome.java

---
+++ LongestPalindrome.java
@@ -0,0 +1,23 @@
+//TC:O(n)
+//SC:O(1)
+class Solution {
+    public int longestPalindrome(String s) {
+        int count = 0;
+        HashSet<Character> set = new HashSet<>();
+
+        for(int i=0; i< s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(set.contains(c)){
+                count+=2;
+                set.remove(c);
+                
+            }else{
+                 set.add(c);
+            }
+        }
+        if(!set.isEmpty()) count++;
+        return count;
+ 
+    }
+}
