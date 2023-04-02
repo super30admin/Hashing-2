d70cdfd77a4b8cdb73246d49d482f751ff052371
AnirudhS30 <127216139+AnirudhS30@users.noreply.github.com>
2023-04-02 10:23:15 -0500

    Update Problem-3.py

--- Problem-3.py
+++ Problem-3.py
@@ -6,19 +6,30 @@ class Solution:
         if not s or len(s)==0:
             return 0
         
-        hashset= set()
-        total=0
-        for i in range (len(s)):
-            # If letter already present in hashset
-            if s[i] in hashset:
-                # Increment total and remove element.
-                total+=2
-                hashset.remove(s[i])
+        hashmap= {}
+        total= 0
+        odd= False
+        for i in s:
+            # Already present.
+            if i in hashmap.keys():
+                hashmap[i]+=1
+            # First entry.
             else:
-                # Else append element.
-                hashset.add(s[i])
+                hashmap[i]=1
         
-        # Check for odd number of occurences.
-        if len(hashset)!=0:
+        for i in hashmap.keys():
+            # If even.
+            if hashmap[i]%2==0:
+                total= total+ 2*hashmap[i]//2
+            # If odd but greater than 2.
+            elif hashmap[i]>2:
+                total= total+ 2*(hashmap[i]-1)//2
+                odd= True
+            # If 1.
+            else:
+                odd= True
+        # Check for odd no.of occurences.
+        if odd:
             total+=1
+        #print(hashmap)
         return total
