c06c7e0ce14ca63f3bcb10b75f699f6159ebccf5
lohitb009 <45014992+lohitb009@users.noreply.github.com>
2023-05-12 22:53:35 -0700

    Submission in C#
    Submission in C#

---
+++ ClassContiguousArray.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeSubmission.Hashing2;
+
+public class ClassContiguousArray
+{
+    public int FindMaxLength(int[] nums)
+    {
+        int rSum = 0;
+        int maxSubarray = 0;
+
+        Dictionary<int, int> rSumDict = new Dictionary<int, int>();
+        rSumDict.Add(0,-1);
+
+        int prvIdx = -2;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0)
+            {
+                // value is 0; subtract
+                rSum -= 1;
+            }
+            else
+            {
+                // value is 1; add
+                rSum += 1;
+            }
+            
+            // chk for the key-value pair
+            if (rSumDict.ContainsKey(rSum) && rSumDict.TryGetValue(rSum,out prvIdx))
+            {
+                maxSubarray = Math.Max(maxSubarray, (i - prvIdx));
+            }
+            else
+            {
+                // seen the rSum for the first-time
+                rSumDict.Add(rSum,i);
+            }
+        }
+        return maxSubarray;
+    }
+}
---
+++ ClassLongestPallindrome.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCodeSubmission.Hashing2;
+
+public class ClassLongestPallindrome
+{
+    public int LongestPalindrome(string s)
+    {
+        int oneCount = 0;
+        int totalCount = 0;
+
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char sChar = s[i];
+            if (charCount.ContainsKey(s[i]))
+            {
+                int prevCount;
+                if (charCount.TryGetValue(sChar, out prevCount))
+                {
+                    // key already exist
+                    charCount[sChar] = prevCount + 1;
+                }
+            }
+            else
+            {
+                // key seen for the first time
+                charCount.Add(sChar,1);
+            }
+        }
+        
+        // iterate the dictionary
+        foreach (KeyValuePair<char, int> pair in charCount)
+        {
+            if (pair.Value % 2 != 0 && pair.Value != 1)
+            {
+                // odd value other than 1
+                totalCount += pair.Value - 1;
+                oneCount += 1;
+                continue;
+            }
+            else if (pair.Value % 2 != 0 && pair.Value == 1)
+            {
+                // odd value equal to 1
+                oneCount += 1;
+                continue;
+            }
+            else
+            {
+                //even count, just add to the total
+                totalCount += pair.Value;
+                continue;
+            }
+        }
+
+        return (totalCount+Math.Min(1,oneCount));
+    }
+}
