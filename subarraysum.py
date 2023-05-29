aa997c77df64c7181cf68011ecd5bf4a2fed43b2
amreenmahenaz <113376911+amreenmahenaz@users.noreply.github.com>
2023-05-29 14:50:29 -0700

    Add files via upload

---
+++ subarraysum.py
@@ -0,0 +1,24 @@
+
+# #Time Complexity :O(n)
+# // Space Complexity :O(n)
+# // Did this code successfully run on Leetcode :YES
+# // Any problem you faced while coding this :NO
+class Solution:
+	def subarraySum(self, nums: List[int], k: int) -> int:
+
+		ans=0
+		prefsum=0
+		d={0:1}
+
+		for num in nums:
+			prefsum = prefsum + num
+
+			if prefsum-k in d:
+				ans = ans + d[prefsum-k]
+
+			if prefsum not in d:
+				d[prefsum] = 1
+			else:
+				d[prefsum] = d[prefsum]+1
+
+		return ans
