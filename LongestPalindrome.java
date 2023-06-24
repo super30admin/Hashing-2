85e62915147bd6b16c27cf327f3569ebad393143
Amey Sunil Kanunje <115860258+ameykanunje@users.noreply.github.com>
2023-06-24 18:10:32 -0400

    Add files via upload

---
+++ LongestPalindrome.java
@@ -0,0 +1,22 @@
+// Time complexity o(n) Space complexity o(1);
+class Solution {
+    public int longestPalindrome(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            char p=s.charAt(i);
+            if(set.contains(p)){
+                count+=2;
+                set.remove(p);
+            }
+            else{
+                set.add(p);
+            }
+
+        }
+        if(!set.isEmpty()){
+            count++;
+        }
+        return count;
+    }
+}
