c69d04d6423f41126a6ac8e4ab82c0f525f947ee
RudranilMitra <62679743+RudranilMitra@users.noreply.github.com>
2023-08-13 21:42:30 -0700

    Update ContigousArray.java

--- ContigousArray.java
+++ ContigousArray.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 class Solution {
     public int findMaxLength(int[] nums) {
         Integer length = 0;
