60d9bf2d3b3917a7f337bddb492b1b12043afa1c
RudranilMitra <62679743+RudranilMitra@users.noreply.github.com>
2023-08-13 21:41:55 -0700

    Create ContigousArray.java

---
+++ ContigousArray.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Integer length = 0;
+        int index = 0;
+        int runningSum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        map.put(0, -1);
+
+        for (int n : nums) {
+            if (n == 1) {
+                runningSum += 1;
+            } else if (n == 0) {
+                runningSum -= 1;
+            }
+            if (!map.containsKey(runningSum)) {
+                map.put(runningSum, index);
+            } else {
+                if (index - map.get(runningSum) > length) {
+                    length = index - map.get(runningSum);
+                }
+            }
+            index++;
+        }
+        
+        return length;
+    }
+}
