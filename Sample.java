d0952b32fb1f2efed0813ae26190adca21747bf7
officio-indrajeet <officio-indrajeet@gmail.com>
2023-08-26 17:57:04 +0530

    testing

--- Sample.java
+++ Sample.java
@@ -3,5 +3,9 @@
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
-
 // Your code here along with comments explaining your approach
+class Sample {
+    public static void main(String[] args) {
+        System.out.println("testing");
+    }
+}
