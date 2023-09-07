b697e145b0dc65864d839c70e8330d9857071e47
UmangThakur15 <umangthakuremail@gmail.com>
2023-09-07 19:34:04 -0400

    Contiguous Array

--- ContiguousArray.java
+++ ContiguousArray.java
@@ -12,19 +12,19 @@ public class ContiguousArray {
         int rsum=0;         //store unique running sum
         int max=0;          //current max value
         map.put(0,-1); //First element of Hash Map
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){         //running through the array TC O(N)
             if(nums[i]==0){   //If value is 0 then subtract from running sum
-                rsum=rsum-1;
+                rsum=rsum-1;      //Comparison and increment take O(1)
             }
             else{
                 rsum=rsum+1;   //As it is binary array so the other element would be 1 and will add 1 to the running sum
             }
 
             if(map.containsKey(rsum)){      //If running sum is already in the HashMap
-                max = Math.max(max, i - map.get(rsum));
+                max = Math.max(max, i - map.get(rsum));     //Look up will take constant time
             }
             else{                   //If it is not there then add it to HashMap
-                map.put(rsum,i);
+                map.put(rsum,i);    //Adding into hashMap take O(1) time
             }
         }
         return max;
