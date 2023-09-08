1633b186bea249074a9e34d9ac5cbf34f4a17899
Jaissica <jaissica.hora@gmail.com>
2023-09-08 01:30:18 -0400

    Update Subarray Sum Equals K-560.java

--- Subarray Sum Equals K-560.java
+++ Subarray Sum Equals K-560.java
@@ -1,1 +1,38 @@
+class Solution {
+    // Time Complexity: O(n)
+    public int subarraySum(int[] nums, int k) {
+        // If array is null return 0
+        if(nums==null || nums.length==0)
+        {
+            return 0;
+        }
+        // The hashmap stores sum and how many time the corresponsing sum value is achieved
+       HashMap<Integer, Integer> map = new HashMap<Integer, Integer> ();
+       int count = 0;
+       int sum = 0;
+       // Zero is added to deal when sum == k 
+       map.put(0,1);
+       for(int i=0;i<nums.length;i++)
+       {
+         sum+=nums[i];
+         int val = sum - k;
 
+         if(map.containsKey(val))
+         {
+           count+=map.get(val);
+         }
+
+        //  if(map.containsKey(sum))
+        //  {
+        //     map.put(sum, map.get(sum)+1);
+        //  }
+        //  else {
+        //      map.put(sum,1);
+        //  }
+        map.put(sum, map.getOrDefault(sum,0)+1);
+       }
+
+       return count;
+
+    }
+}
