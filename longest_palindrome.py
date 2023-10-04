b604522cc8979b30e74976206e1224688ff605f7
Merin Joseph <merinjputhiyedath@gmail.com>
2023-10-04 00:09:12 -0700

    modified changes needed after class

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -1,5 +1,5 @@
 # Time Complexity : O(n)
-# Space Complexity : O(n)
+# Space Complexity : O(1)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : took some time thats it
 
--- subarray_sum.py
+++ subarray_sum.py
@@ -3,28 +3,26 @@
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : Had to look up synatx of dictionary in python online
 
-
-# Loop through the array to see how many subarrays actually sum to k
-# If sum-k is already present in the frequencies dictionary, then increase the count value by the respective frequency
-# If sum in frequencies dict, increase the frequency count by 1 for that value respec. if not, add the frequency of that sum key to 1
+# Loop through the array to see how many subarrays actually rsum to k
+# If rsum-k is already present in the frequencies dictionary, then increase the count value by the respective frequency
+# If rsum in frequencies dict, increase the frequency count by 1 for that value respec. if not, add the frequency of that rsum key to 1
 k=int(input("enter the value of k: "))
 array_str=input("enter the array of integers seperated by spaces: ")
 array_list=array_str.split()
 nums=[int(item) for item in array_list]
-
-sum=0
+rsum=0
 count=0 
 freq={0:1}
 
 for num in nums:
-    sum+=num
-    if((sum-k) in freq):
-        count+=freq[sum-k]
+    rsum+=num
+    if((rsum-k) in freq):
+        count+=freq[rsum-k]
     
-    if(sum in freq):
-        freq[sum]+=1
+    if(rsum in freq):
+        freq[rsum]+=1
     else:
-        freq[sum]=1
+        freq[rsum]=1
 print(count)
 
 
