b365724eade6dbdfa579e7741e1c6b607ad03d50
Dibyajyoti Biswal <dj_code12@Dibyajyotis-Air.lan>
2023-12-27 17:48:17 -0600

    Problem 18 done

---
+++ Problem18_1.py
@@ -0,0 +1,23 @@
+'''
+Time complexity: O(n)
+Space complexity: O(n)
+'''
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        rSum, HashMap = 0, {0: -1}
+        Res, diff = 0, 0
+
+        for i in range(len(nums)):
+            if (nums[i]==0):
+                rSum-=1
+            else:
+                rSum+=1
+            if rSum in HashMap:
+                diff = i-HashMap[rSum]
+                if diff > Res:
+                    Res = diff
+            else:
+                HashMap[rSum] = i # Store earliest index as happened for first time
+
+        return Res
+        
