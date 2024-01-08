82427c687b432ee3747af62baf80c198ca1c708a
Kripali Bhatodra <kripalibhatodra2206@gmail.com>
2024-01-07 17:08:08 -0800

    Hashing-2

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,24 @@
+// Time Complexity :o(n)
+// Space Complexity :
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> Map= new HashMap<>();
+        Map.put(0,1);
+        int sum=0;
+        int count =0;
+        for(int i=0; i<nums.length;i++){
+            sum+=nums[i];
+            if(Map.containsKey(sum-k)){
+                count=count+Map.get(sum-k);
+            }
+            if(!Map.containsKey(sum)){
+                Map.put(sum,0);
+            }
+            Map.put(sum,Map.get(sum)+1);
+        }
+        return count;
+    }
+}
