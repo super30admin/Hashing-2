498f79b4b71abd4ca8aded24f152d4af9de4e57a
Sriram Akella <Sriram.Akella@gmail.com>
2024-02-12 15:48:02 -0700

    subarraysum complete

---
+++ subArraySum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        count = 0
+        rsum = 0
+        hmap = {0: 1}
+        for i in range(n):
+            rsum = rsum + nums[i]
+            if (rsum - k) in hmap:
+                count = count + hmap[rsum - k]
+            if (rsum) in hmap:
+                hmap[rsum] = hmap.get(rsum) + 1
+            else:
+                hmap[rsum] = 1
+        return count
