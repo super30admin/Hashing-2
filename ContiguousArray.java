94e8bfc3291786d4a00b92645f99347367ba193a
AryanReddyy <aryan02reddy@gmail.com>
2024-03-15 15:24:09 -0400

    Hashing-2

--- ContiguousArray.java
+++ ContiguousArray.java
@@ -1,3 +1,28 @@
-// TC :
-// SC :
+// TC : O(n)
+// SC : O(n)
 
+import java.util.HashMap;
+
+public class ContiguousArray {
+    public int findMaxLength(int[] nums) {
+
+        if (nums == null || nums.length == 0) return -1;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int max = 0;
+        int rSum =0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                rSum--;
+            } else rSum++;
+            
+            if(map.containsKey(rSum)) {
+                max = Math.max(max, i-map.get(rSum));
+            } else map.put(rSum, i);
+        }
+        
+        return max;
+    }
+}
